01dd2aa5061152496da8b6ae23bfce82eed662b2
panesofglass <ryan.riley@panesofglass.org>
2014-07-01 00:23:09 -0500

    Original template

---
+++ FSharpWeb1/Controllers/CarsController.fs
@@ -0,0 +1,16 @@
+namespace FSharpWeb1.Controllers
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Net.Http
+open System.Web.Http
+open FSharpWeb1.Models
+
+/// Retrieves values.
+type CarsController() =
+    inherit ApiController()
+
+    let values = [| { Make = "Ford"; Model = "Mustang" }; { Make = "Nissan"; Model = "Titan" } |]
+
+    /// Gets all values.
+    member x.Get() = values
---
+++ FSharpWeb1/Models/Car.fs
@@ -0,0 +1,9 @@
+namespace FSharpWeb1.Models
+
+open Newtonsoft.Json
+
+[<CLIMutable>]
+type Car = {
+    Make : string
+    Model : string
+}
